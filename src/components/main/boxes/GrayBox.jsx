3730b561ba0ce77f6667b3e71e99b68c7e1c5fa2
flavioandrade91 <flavio.andrade91@outlook.com.br>
2024-04-29 02:01:40 -0300

    modificações player

---
+++ src/components/main/boxes/GrayBox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './GrayBox.module.css'
+
+export function GrayBox(){
+    return(
+        <>
+            <section className={styles.boxContainer}>
+                dasdsad
+            </section>
+        </>
+    );
+}
