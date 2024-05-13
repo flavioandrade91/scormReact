e362aaefc60c7cf3a183453c67134e23061c43cb
flavioandrade91 <flavio.andrade91@outlook.com.br>
2024-05-13 00:23:52 -0300

    questionario

--- src/components/questionario/Questionario.jsx
+++ src/components/questionario/Questionario.jsx
@@ -1,24 +1,66 @@
 import React from 'react'
 import styles from './Questinario.module.css'
 import flag from '../../assets/IconFlag.svg'
+import ImgO1Quest from '../../assets/Img01Quest.svg'
+import ImgO2Quest from '../../assets/Img02Quest.svg'
+import ImgO3Quest from '../../assets/Img03Quest.svg'
 
 export function Questionario() {
     return (
+
         <section className={styles.questionarioContent}>
-            <div className={styles.headerQuestionario}>
-                <div>
-                    <span className={styles.title}>titulo</span>
-                    <span className={styles.subtitle}>subtitle</span>
+
+            <section className={styles.headerContent}>
+
+                <div className={styles.headerTitle}>
+                    <div className={styles.title}>
+                        <span>exercício de fixação</span>
+                    </div>
+                    <div className={styles.subtitle}>
+                        <span>alternativa correta</span>
+                    </div>
                 </div>
 
-                <img src={flag} alt="" srcset="" />
+                <div className={styles.flagBox}>
+                    <div className={styles.flag}>
+                        <img src={flag} alt="" />
+                    </div>
+                </div>
 
-            </div>
+            </section>
+
 
             <div className={styles.questionario}>
 
-                <span>testetetstdte</span>
+                <span className={styles.question}>
+                    <p> <span>1)</span> Estudamos os tipos de projetos de produção de moda:
+                       projetos de imagem, de exposição de produtos e eventos
+                       de moda, cada um possuindo suas variações e aplicações.
+                    </p>
 
+                    <p>De acordo com o conteúdo estudado, associe cada uma das
+                       imagens com o tipo de projeto de produção de moda correto:
+                    </p>      
+                </span>
+
+                <div className={styles.boxImages}>
+                    <div className={styles.images}>
+                    <img src={ImgO1Quest} alt="" srcset="" />
+                    <span>1</span>
+                    </div>
+                    <div className={styles.images}>
+                    <img src={ImgO2Quest} alt="" srcset="" />
+                    <span>2</span>
+                    </div>
+                    <div className={styles.images}>
+                    <img src={ImgO3Quest} alt="" srcset="" />
+                    <span>3</span>
+                    </div>
+                   
+                </div>
+
+                <div className={styles.quetions}>
+                    
                 <label htmlFor="">questão 1</label>
                 <input type="radio" name="" id="" />
 
@@ -32,6 +74,8 @@ export function Questionario() {
                 <input type="radio" name="" id="" />
 
                 <button type="submit">Enviar</button>
+                </div>
+
             </div>
 
         </section>
